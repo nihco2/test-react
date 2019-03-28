aac251e9b0c1713b908307ae26ff481dcd6d9143
Nicolas Martin <nihco2@gmail.com>
2019-03-28 22:38:55 +0100

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      shops: null
+     }
+  }
+  async componentDidMount() {
+    try {
+      const result = await fetch('https://www.leshabitues.fr/testapi/shops', {
+        headers: {
+          Accept: 'application/json',
+        }
+      })
+      const data = await result.json();
+      console.log(data)
+      this.setState({
+        shops: data.results
+      })
+    }
+    catch(err) {
+      console.log(err);
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
